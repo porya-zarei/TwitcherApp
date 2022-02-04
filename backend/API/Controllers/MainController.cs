9f83fa46fb605d62ffe9de138d351b05226331ee
Porya Zarei <pzeinstein@gmail.com>
2022-02-04 22:27:00 +0330

    initial backend

---
+++ backend/API/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MainController : ControllerBase
+{
+    private readonly ILogger<MainController> _logger;
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+}
---
+++ backend/API/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using System;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
